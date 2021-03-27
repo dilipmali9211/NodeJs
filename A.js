ded5173cbd588f5f151bc877b3efd9111485e8d4
dilipmali <dilipmali9211@gmail.com>
2021-03-27 08:45:44 +0530

    26march

---
+++ A.js
@@ -0,0 +1,33 @@
+console.log('hello world');
+
+//varible
+var a;
+a=4;
+var b;
+b=7;
+console.log(a+b);
+let c=2,d=5;
+console.log(c*d);
+const e=25,f=5;
+console.log(e%f);
+//function
+function fun(){
+     console.log("hello world");
+}
+function fun1(){
+    var a1=2,b1=3;
+    console.log(a1+b1);
+}
+function fun2(){
+    let a2=4,b2=2;
+    console.log(a2*b2);
+}
+function fun3(){
+    const a3=5,b3=8;
+    console.log(a3$a3);
+}
+//function calling
+fun();
+fun1();
+fun2();
+fun3();
