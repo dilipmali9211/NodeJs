1611a542ac12f11fcea07b0fa5b4d1fc29b42540
dilipmali <dilipmali9211@gmail.com>
2021-03-30 16:13:54 +0530

    30march

---
+++ I.js
@@ -0,0 +1,7 @@
+function fun2(){
+    var a=67,g=78
+    console .log("first line is here:-"+(a+g))
+    console .log('second line is here:-'+a+g);
+    console .log(`thrad line is here:- ${a+g}`);
+}
+fun2()
