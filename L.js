852f03a220f6bcd3e24f282af57c6945fe82fea8
dilipmali <dilipmali9211@gmail.com>
2021-03-30 16:50:39 +0530

    30march

---
+++ L.js
@@ -0,0 +1,11 @@
+class SumClass{
+    // a=12,b3
+    SumClass(){
+        let a=12,b=3
+    console .log(a*b)
+}
+}
+let obj = new SumClass();
+obj.SumClass();
+
+//we can't define varible in class body
