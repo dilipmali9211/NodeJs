cbc10df7131f51f35d997411862f75f3ada740f4
dilipmali <dilipmali9211@gmail.com>
2021-03-31 08:58:13 +0530

    peramater

---
+++ O.js
@@ -0,0 +1,13 @@
+//creating peramater
+class MyClass{
+    //Proprty;
+    
+    //conscutor;
+    
+    //Method;
+    myFunction(c){ // C is a formal peramater
+        console .log(`This file name is :- ${c}`);
+    }
+    } //Class Ending
+    let x = new  MyClass();
+    x.myFunction("NodeJs"); //"NodeJs -> is actul argument"
