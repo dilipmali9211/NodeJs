87e9120ba211ae7f7b05ddda8324cdf28a3f3999
dilipmali <dilipmali9211@gmail.com>
2021-03-31 08:40:48 +0530

    creating class

---
+++ M.js
@@ -0,0 +1,15 @@
+//Creating Class in function
+class MyClass{
+    //Proprti
+
+    //Constructor
+
+    //Method
+    myFunction(){
+        console .log("My first function:-");
+    }// Class defination end;
+}
+//Class calling;
+let x = new MyClass();
+//Function calling;
+x.myFunction();
---
+++ N.js
@@ -0,0 +1,19 @@
+//Creating  one of more method in class
+class MyClass{
+    //Proprty;
+
+    //Constructor;
+
+    //Method
+    myfunction1(){
+        console.log('My first function');
+    }
+    myfunction(){
+console .log('My second function');
+    }
+} //Class Ending
+//Class Calling;
+let x = new MyClass();
+//function calling
+x.myfunction1();
+x.myfunction();
