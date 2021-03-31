9189a66cecae05812f13a79c8722a2c0b8a63e4e
dilipmali <dilipmali9211@gmail.com>
2021-03-31 09:07:28 +0530

    this. key word

---
+++ P.js
@@ -0,0 +1,17 @@
+//USE OF 'This.' KEYWORD
+class MyClass{
+    //Proprty
+
+    //Constructor
+
+    //Method
+    myFunction(){
+        console .log('EXAMPLE OF THIS KEY WORD');
+        this.myFunction1();
+    }
+    myFunction1(){
+        console .log('exm of this key word');
+    }//function ending
+}//class ending
+let s = new MyClass();
+s.myFunction();
