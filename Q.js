0b018beeb16a94b7bf302d64b6df5b4f5df5a1ec
dilipmali <dilipmali9211@gmail.com>
2021-04-01 09:01:09 +0530

    inheritance

---
+++ Q.js
@@ -0,0 +1,350 @@
+//Inhritance
+class MyClassA{
+    //1.proprties
+
+    //2.constructor
+    constructor(){
+        
+    }
+
+    //3.method
+    myFunctiona(){
+        console .log('this file number is:-2');
+    }
+}
+class MyClassB extends MyClassA{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionb(){
+        console .log('this file number is:-3');
+    }
+}
+class MyClassC extends MyClassB{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+           super();
+    }
+
+    //3.method
+    myFunctionc(){
+        console .log('this file number is:-4');
+    }
+}
+class MyClassD extends MyClassC{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctiond(){
+        console .log('this file number is:-5');
+    }
+}
+class MyClassE extends MyClassD{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctione(){
+        console .log('this file number is:-6');
+    }
+}
+class MyClassF extends MyClassE{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionf(){
+        console .log('this file number is:-7');
+    }
+}
+class MyClassG extends MyClassF{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctiong(){
+        console .log('this file number is:-8');
+    }
+}
+class MyClassH extends MyClassG{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionh(){
+        console .log('this file number is:-9');
+    }
+}
+class MyClassI extends MyClassH{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctioni(){
+        console .log('this file number is:-10');
+    }
+}
+class MyClassJ extends MyClassI{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionj(){
+        console .log('this file number is:-11');
+    }
+}
+class MyClassK extends MyClassJ{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionk(){
+        console .log('this file number is:-12');
+    }
+}class MyClassL extends MyClassK{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionl(){
+        console .log('this file number is:-13');
+    }
+}
+class MyClassM extends MyClassL{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionm(){
+        console .log('this file number is:-14');
+    }
+}
+class MyClassN extends MyClassM{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionn(){
+        console .log('this file number is:-15');
+    }
+}
+class MyClassO extends MyClassN{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctiono(){
+        console .log('this file number is:-16');
+    }
+}
+class MyClassP extends MyClassO{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionp(){
+        console .log('this file number is:-17');
+    }
+}
+class MyClassQ extends MyClassP{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionq(){
+        console .log('this file number is:-18');
+    }
+}
+class MyClassR extends MyClassQ{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionr(){
+        console .log('this file number is:-19');
+    }
+}
+class MyClassS extends MyClassR{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctions(){
+        console .log('this file number is:-20');
+    }
+}
+class MyClassT extends MyClassS{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctiont(){
+        console .log('this file number is:-21');
+    }
+}
+class MyClassU extends MyClassT{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionu(){
+        console .log('this file number is:-22');
+    }
+}
+class MyClassW extends MyClassU{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionw(){
+        console .log('this file number is:-23');
+    }
+}
+class MyClassX extends MyClassW{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionx(){
+        console .log('this file number is:-24');
+    }
+}
+class MyClassY extends MyClassX{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctiony(){
+        console .log('this file number is:-25');
+    }
+}
+class MyClassZ extends MyClassY{
+    //1.proprties
+    
+    //2.constructor
+    constructor(){
+        super();
+    }
+
+    //3.method
+    myFunctionz(){
+        console .log('this file number is:-26');
+    }
+}
+let abc = new MyClassZ();
+abc.myFunctiona();
+abc.myFunctionb();
+abc.myFunctionc();
+abc.myFunctione();
+abc.myFunctionf();
+abc.myFunctiong();
+abc.myFunctionh();
+abc.myFunctioni();
+abc.myFunctionj();
+abc.myFunctionk();
+abc.myFunctionl();
+abc.myFunctionm();
+abc.myFunctionn();
+abc.myFunctiono();
+abc.myFunctionp();
+abc.myFunctionq();
+abc.myFunctionr();
+abc.myFunctions();
+abc.myFunctiont();
+abc.myFunctionu();
+abc.myFunctionw();
+abc.myFunctionx();
+abc.myFunctiony();
+abc.myFunctionz();
