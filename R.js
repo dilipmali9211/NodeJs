df57b61055aaac8e2ae757ebc9babf576f4ddf30
dilipmali <dilipmali9211@gmail.com>
2021-04-02 14:23:32 +0530

    construstor exm.

--- R.js
+++ R.js
@@ -1,12 +1,19 @@
 //class defination
 class MyA{
     //1.proprtis
-    
+      name='';  //empty variable
     //2.constructor
+    constructor(){
+        this.name="constructor exm."
+        this.myFunction2();
+    }
 
     //3.method
     myFunction(v){ //formal permetar,
         console .log(`This is a example of constructor.${v}`);
+    }
+    myFunction2(){
+        console .log(` hello ${this.name}`);
     }
 
 }//class end
