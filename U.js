7f62089f456e8f4ed6719cf07d4749ab03da4dd8
dilipmali <dilipmali9211@gmail.com>
2021-04-02 16:05:15 +0530

    exm1

---
+++ U.js
@@ -0,0 +1,14 @@
+class MyClass{
+    //1.proprties
+        student={} //empty varible
+    //2.constrctor
+        constructor(B){ //formal arg.
+            console .log(`hello ${B}`);
+        };
+    //3.method
+        myFun(){
+            console .log("dilip");
+        };
+}
+let z = new MyClass('anil');
+z.myFun("dilip");
