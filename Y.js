3cd4b036d8ef734062b3da3c7901a559745f6947
dilipmali <dilipmali9211@gmail.com>
2021-04-03 12:16:39 +0530

    varible in obj

---
+++ Y.js
@@ -0,0 +1,13 @@
+let D={
+    name:"dilip mali",
+    "class":"MCA 5th",
+    "collage":"gyanodya insitutde of managemant",
+    mob:"1234567890",
+getmob:function(){
+    return this.mob;
+}
+};
+console.log(D.name);
+console.log(D.class);
+console.log(D.collage);
+console.log(D.getmob());
